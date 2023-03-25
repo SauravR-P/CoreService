5673824be4463656bcadeb8688f5a2ac4d07de89
SauravR-P <sauravrp1998@gmail.com>
2023-03-25 11:41:07 +0530

    Add project files.

---
+++ CoreService/Repository/IDbCrudOperations.cs
@@ -0,0 +1,9 @@
+﻿namespace CoreService.Repository
+{
+    public interface IDbCrudOperations
+    {
+        Task<T> CreateAsync<T>(T entity);
+        Task<T> UpdateAsync<T>(int Id);
+        Task<T> DeleteAsync<T>(int Id);
+    }
+}
---
+++ CoreService/Service/DbCrudOperations.cs
@@ -0,0 +1,22 @@
+﻿using CoreService.Repository;
+
+namespace CoreService.Service
+{
+    public class DbCrudOperations : IDbCrudOperations
+    {
+        public Task<T> CreateAsync<T>(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> DeleteAsync<T>(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync<T>(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
